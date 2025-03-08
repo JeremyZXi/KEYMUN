a64d71bcc9fbc6fac2cefabb794b755d936c259a
JeremyZXi <zjeremy0710@gmail.com>
2025-03-08 21:40:54 +0800

    about

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -54,7 +54,7 @@ const Footer = () => {
 
                 <div className="mt-8 pt-8 border-t border-gray-700">
                     <p className="text-sm text-center">
-                        KEYMUN 2025
+                        KEYMUN 2025. All Rights Reserved
                     </p>
                 </div>
 
